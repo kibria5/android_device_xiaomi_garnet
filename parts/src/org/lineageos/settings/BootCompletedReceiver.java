53e0c044e523f090621494ce867b897a857391aa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-11 01:42:11 +0530

    garnet: Override HDR types for HDR
    on stock this device used to advertise hdr capabilities with miui fw and
    blobs but this changed with hyperos they only report dolby vision now,
    lets override HdrCapabilities till we find a proper way to bring it back
    
    Change-Id: I93ecfbd7aef42e5e1a4c696e196e482a023c9ae4

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,7 +20,11 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
+import android.os.UserHandle;
 import android.util.Log;
+import android.view.Display.HdrCapabilities;
+import android.view.SurfaceControl;
 
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
@@ -38,5 +42,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         Log.i(TAG, "Boot completed, starting services");
         RefreshUtils.startService(context);       
         ThermalUtils.startService(context); 
+        
+        // Override HDR types to enable HDR
+        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
+        SurfaceControl.overrideHdrTypes(displayToken,
+                new int[] {HdrCapabilities.HDR_TYPE_HDR10,HdrCapabilities.HDR_TYPE_HLG,
+                        HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }
