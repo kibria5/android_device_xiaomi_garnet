bbf078df208de8f5077cb1f9d0536b493afc2ae5
Andrzej Szafran <rtd1250@gmail.com>
2024-02-18 15:51:48 +0100

    garnet: Import QTI power HAl
    * from https://github.com/LineageOS/android_vendor_qcom_opensource_power/tree/lineage-21.0
    
    Change-Id: Ice768d4a7bef2e102b6a5d5bc86ecd7594e75bab

--- device.mk
+++ device.mk
@@ -427,7 +427,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti-sm7435
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.3.vendor
