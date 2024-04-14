81b066f27b90f50d8b1dcac936d691085a2bf739
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-04-14 18:06:04 +0530

    garnet: parts: Move to device encrypted storage
    Kill unnecessary explicit app version while we're at it.
    
    Change-Id: I678e3598b28b57e1920da584102d8c526ab452ef

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -31,9 +31,12 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
+        Log.d(TAG, "Received intent: " + intent.getAction());
+        if (!intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
+            return;
+        }
+        Log.i(TAG, "Boot completed, starting services");
+        RefreshUtils.startService(context);       
+        ThermalUtils.startService(context); 
     }
 }
