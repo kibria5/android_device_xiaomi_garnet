bc71b163866f234d6b65b1ea2f9e51d23bc17eaf
Ruchit <risenid@duck.com>
2024-09-30 15:17:34 +0530

    garnet: Build libartpalette
    F linker  : CANNOT LINK EXECUTABLE "/apex/com.android.art/bin/dex2oat64": library "libartpalette.so" not found: needed by main executable
    
    Change-Id: Icad8b6468c6ef43e6c8ac3b49705b6f9d6e5e77b
    Signed-off-by: Ruchit <risenid@duck.com>
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- device.mk
+++ device.mk
@@ -289,6 +289,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# libartpalette
+PRODUCT_HOST_PACKAGES += \
+    libartpalette
+
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
