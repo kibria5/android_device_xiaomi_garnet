990a0c7ef4f5de8f468d6b3aaa1f0bf6eb01a5c7
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2024-12-20 05:59:37 +0000

    garnet: Preopt Settings & NexusLauncherRelease.

--- device.mk
+++ device.mk
@@ -293,6 +293,8 @@ PRODUCT_COPY_FILES += \
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += Settings
+PRODUCT_DEXPREOPT_SPEED_APPS += NexusLauncherRelease
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
