77e72787b8d943fdd946ea15c9e9e8a1cb987bc9
Mohammad Kibria <106745675+kibria5@users.noreply.github.com>
2025-02-15 08:32:17 +0000

    garnet: Preopt Launcher3QuickStep

--- device.mk
+++ device.mk
@@ -310,6 +310,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
 PRODUCT_DEXPREOPT_SPEED_APPS += Settings
 PRODUCT_DEXPREOPT_SPEED_APPS += NexusLauncherRelease
+PRODUCT_DEXPREOPT_SPEED_APPS += Launcher3QuickStep
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
